27dd83350047b718b34802857d10fb5726a984d4
AishwaryaS03 <aishwarya.s@caratlane.com>
2023-01-25 10:45:16 +0530

    basicexample in retrofit

---
+++ app/src/main/java/com/example/basicretrofitexample/ApiInterface.kt
@@ -0,0 +1,12 @@
+package com.example.basicretrofitexample
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiInterface {
+
+    @GET(value = "posts")
+
+    fun getData(): Call<List<MyDataItem>>
+
+}
---
+++ app/src/main/java/com/example/basicretrofitexample/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.basicretrofitexample
+
+class MyData : ArrayList<MyDataItem>()
